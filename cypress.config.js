5a8afb23fdd06b923645e575bde14b825f35f3d8
Duyen Nguyen <thuduyen07@gmail.com>
2024-11-29 00:21:28 +0700

    update config and libraries

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    "baseUrl": "https://demoqa.com",
+    "specPattern": "./cypress/tests/**.*" ,
+    "defaultCommandTimeout": 40,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  "projectId": "CypressPractice",
-  "specPattern": "./cypress/tests/**.*" ,
-  "baseUrl": "https://demoqa.com",
+
+  "projectId": 's9jp81',
+  video: true,
+  videoCompression: true,
 });
